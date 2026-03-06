cc4ebac8d7358c50e770c5d8559c58f34e753670
MayZermeno <92339259+MayZermeno@users.noreply.github.com>
2026-03-06 17:36:57 -0600

    actualización mensaje importante

--- src/components/ImportantInfo.jsx
+++ src/components/ImportantInfo.jsx
@@ -12,10 +12,13 @@ export default function ImportantInfo() {
       fontWeight: '500',
       textAlign:"center"
     }}>
-      <strong>Importante:</strong><br />
-      Esta celebración es una <strong>fiesta sorpresa</strong>.<br />
-      Magdis cree que es ella quien está organizando el festejo, por lo que te pedimos <strong>máxima discreción</strong> para mantener la magia hasta el gran momento.<br />
-      Para asegurar que todo salga perfecto y sea inolvidable. 🤫✨
+<strong>Importante:</strong><br />
+Esta celebración es una <strong>fiesta sorpresa</strong>.<br />
+Magdis cree que es ella quien está organizando la fiesta y por eso está mencionando a algunas personas <strong>otra fecha diferente</strong>.<br />
+<strong>Por favor no te confundas:</strong> la fecha correcta es la que aparece en esta invitación. 
+Si Magdis te menciona otra fecha, solo <strong>síguele la corriente</strong> para no arruinar la sorpresa.<br />
+Te pedimos <strong>máxima discreción</strong> para mantener la magia hasta el gran momento.<br />
+Queremos asegurarnos de que todo salga perfecto y sea inolvidable. 🤫✨
     </div>
   );
 }
